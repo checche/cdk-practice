b290a323518971a0c0c5e9153482b273b72fe9d7
Takahiro Hirata <ta.hirata@opt.ne.jp>
2020-07-17 16:35:30 +0900

    add: testing lambda

--- cdk-workshop/test/hitcounter.test.ts
+++ cdk-workshop/test/hitcounter.test.ts
@@ -16,3 +16,26 @@ test("DynamoDB Table Created", () => {
 
   expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
 });
+
+test("Lambda Has Environment Variables", () => {
+  const stack = new cdk.Stack();
+
+  new HitCounter(stack, "MyTestConstruct", {
+    downstream: new lambda.Function(stack, "TestFunction", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "lambda.handler",
+      code: lambda.Code.inline("test"),
+    }),
+  });
+
+  expectCDK(stack).to(
+    haveResource("AWS::Lambda::Function", {
+      Environment: {
+        Variables: {
+          DOWNSTREAM_FUNCTION_NAME: { Ref: "TestFunction22AD90FC" }, // 一旦テスト実行して実際の値を確認して書く。
+          HITS_TABLE_NAME: { Ref: "MyTestConstructHits24A357F0" },
+        },
+      },
+    })
+  );
+});
